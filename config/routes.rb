40528c876c08c3817f36fd55ed9d676f7d5b972c
Ihor Breza <seniorigor@gmail.com>
2015-02-02 00:00:10 +0200

    Users CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   scope "/admin" do
-    resources :users, only: [:index, :show]
+    resources :users
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
